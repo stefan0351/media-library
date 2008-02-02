1ca109fc2b5386a4b9b4f22b7e3405307af3ccf6
Stefan Stiller <stefan74@sstiller.de>
2008-02-02 20:18:47 +0000

    Reworked DVD cover creation
    git-svn-id: svn://kiwinb/dev/media/trunk@355 d2417a56-bda1-9d42-b968-744157609d61

---
+++ core/src/com/kiwisoft/utils/ODFTemplate.java
@@ -0,0 +1,145 @@
+package com.kiwisoft.utils;
+
+import java.io.*;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+import java.awt.Dimension;
+
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.commons.io.*;
+import org.apache.commons.lang.StringEscapeUtils;
+
+import sun.net.www.MimeEntry;
+import sun.net.www.MimeTable;
+import com.kiwisoft.media.pics.PictureUtils;
+
+/**
+ * @author Stefan Stiller
+ */
+public class ODFTemplate
+{
+	private URL resource;
+	private Map<String, Object> variables=new HashMap<String, Object>();
+	private Map<String, File> pictures=new HashMap<String, File>();
+	public double maxWidth=12.7;
+	public double maxHeight=18.0;
+	private static File file;
+
+	public static void main(String[] args) throws Exception
+	{
+		ODFTemplate template=new ODFTemplate(ClassLoader.getSystemResource("com/kiwisoft/media/covers/dvd_movie.odtt"));
+		template.setVariable("title", "The Last Mimzy");
+		template.setPicture("poster", new File("D:\\Webpages\\local\\movies\\posters\\last_mimzy.jpg"));
+		file=new File("d:\\temp\\test.odt");
+		template.createDocument(file);
+	}
+
+	public ODFTemplate(URL resource) throws IOException
+	{
+		this.resource=resource;
+	}
+
+	public void setPicture(String name, File file)
+	{
+		pictures.put(name, file);
+		Map<String, Object> pictureVariables=new HashMap<String, Object>();
+		String fileName=file.getName();
+		pictureVariables.put("filename", fileName);
+		MimeEntry mimeEntry=MimeTable.getDefaultTable().findByFileName(fileName);
+		if (mimeEntry!=null) pictureVariables.put("mimetype", mimeEntry.getType());
+		Dimension size=PictureUtils.getImageSize(file);
+		double scale=Math.min(maxWidth/size.width, maxHeight/size.height);
+		pictureVariables.put("width", size.width*scale);
+		pictureVariables.put("height", size.height*scale);
+		variables.put(name, pictureVariables);
+	}
+
+	public void setVariable(String name, Object value)
+	{
+		if (value!=null) value=StringEscapeUtils.escapeXml(value.toString());
+		variables.put(name, value);
+	}
+
+	public void createDocument(File file) throws Exception
+	{
+//		System.out.println("variables = "+variables);
+		VelocityEngine engine=new VelocityEngine();
+		engine.init();
+		VelocityContext context=new VelocityContext(variables);
+
+		ZipEntry templateEntry;
+		ZipInputStream zipTemplate=new ZipInputStream(resource.openStream());
+		ZipOutputStream zipOutput=new ZipOutputStream(new FileOutputStream(file));
+		while ((templateEntry=zipTemplate.getNextEntry())!=null)
+		{
+			if ("mimetype".equals(templateEntry.getName()))
+			{
+//				System.out.println(templateEntry.getName());
+				int size=(int)templateEntry.getSize();
+				byte[] mimetype=new byte[size];
+				zipTemplate.read(mimetype, 0, size);
+				ZipEntry mimeEntry=new ZipEntry(templateEntry);
+				zipOutput.putNextEntry(mimeEntry);
+				zipOutput.write(mimetype);
+			}
+		}
+		zipTemplate.close();
+
+		zipTemplate=new ZipInputStream(resource.openStream());
+		while ((templateEntry=zipTemplate.getNextEntry())!=null)
+		{
+			if (!"mimetype".equals(templateEntry.getName()))
+			{
+//				System.out.println(templateEntry.getName());
+				ZipEntry outputEntry=new ZipEntry(templateEntry.getName());
+				if (!templateEntry.isDirectory() && templateEntry.getSize()>0)
+				{
+					int size=(int)templateEntry.getSize();
+					byte[] buffer=new byte[size];
+					int position=0;
+					int bytesRead;
+					while (true)
+					{
+						bytesRead=zipTemplate.read(buffer, position, size-position);
+						if (bytesRead==-1) break;
+						position+=bytesRead;
+						if (position>=size) break;
+					}
+//					System.out.println(new String(buffer));
+					ByteArrayOutputStream output=new ByteArrayOutputStream();
+					OutputStreamWriter writer=new OutputStreamWriter(output);
+					engine.evaluate(context, writer, "ODFTemplate", new InputStreamReader(new ByteArrayInputStream(buffer)));
+					writer.close();
+					byte[] bytes=output.toByteArray();
+//					System.out.println("bytes.length = "+bytes.length);
+					outputEntry.setSize(bytes.length);
+					outputEntry.setMethod(ZipEntry.DEFLATED);
+					zipOutput.putNextEntry(outputEntry);
+					zipOutput.write(bytes);
+					zipOutput.closeEntry();
+				}
+				else
+				{
+					outputEntry.setSize(0);
+					zipOutput.putNextEntry(outputEntry);
+				}
+			}
+		}
+		zipTemplate.close();
+
+		for (Map.Entry<String, File> entry : pictures.entrySet())
+		{
+			ZipEntry pictureEntry=new ZipEntry("Pictures/"+entry.getValue().getName());
+			pictureEntry.setSize(entry.getValue().length());
+			pictureEntry.setMethod(ZipEntry.DEFLATED);
+			zipOutput.putNextEntry(pictureEntry);
+			IOUtils.copy(new FileInputStream(entry.getValue()), zipOutput);
+		}
+		zipOutput.close();
+	}
+}
---
+++ gui/src/com/kiwisoft/media/medium/CreateCoverAction.java
@@ -0,0 +1,88 @@
+package com.kiwisoft.media.medium;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kiwisoft.swing.actions.SimpleContextAction;
+import com.kiwisoft.swing.GuiUtils;
+import com.kiwisoft.utils.ODFTemplate;
+import com.kiwisoft.utils.FileUtils;
+import com.kiwisoft.utils.StringUtils;
+import com.kiwisoft.utils.Utils;
+import com.kiwisoft.app.ApplicationFrame;
+import com.kiwisoft.collection.Chain;
+import com.kiwisoft.media.movie.Movie;
+import com.kiwisoft.media.MediaConfiguration;
+import com.kiwisoft.media.LanguageManager;
+import com.kiwisoft.media.person.CreditType;
+import com.kiwisoft.media.person.CastMember;
+import com.kiwisoft.media.person.Person;
+
+/**
+ * @author Stefan Stiller
+ */
+public class CreateCoverAction extends SimpleContextAction
+{
+	private ApplicationFrame frame;
+
+	public CreateCoverAction(ApplicationFrame frame)
+	{
+		super(Medium.class, "Create Cover");
+		this.frame=frame;
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		try
+		{
+			Medium medium=(Medium)getObject();
+			Chain<Track> tracks=medium.getTracks();
+			if (tracks.size()==1)
+			{
+				Track track=tracks.getFirst();
+				if (track.getMovie()!=null)
+				{
+					Movie movie=track.getMovie();
+					ODFTemplate template=new ODFTemplate(ClassLoader.getSystemResource("com/kiwisoft/media/covers/dvd_movie.odtt"));
+					template.setVariable("key", medium.getFullKey());
+					template.setVariable("title", medium.getName());
+					template.setPicture("poster", FileUtils.getFile(MediaConfiguration.getRootPath(), movie.getPoster().getFile()));
+					String summary=movie.getSummaryText(track.getLanguage());
+					if (StringUtils.isEmpty(summary)) summary=movie.getSummaryText(LanguageManager.getInstance().getLanguageBySymbol("de"));
+					if (StringUtils.isEmpty(summary)) summary=movie.getSummaryText(LanguageManager.getInstance().getLanguageBySymbol("en"));
+					template.setVariable("description", summary);
+					List<CastMember> castMembers=new ArrayList<CastMember>(movie.getCastMembers(CreditType.MAIN_CAST));
+					Collections.sort(castMembers, new CastMember.CreditComparator());
+					StringBuilder cast=new StringBuilder();
+					for (CastMember castMember : castMembers)
+					{
+						Person actor=castMember.getActor();
+						if (actor!=null)
+						{
+							if (cast.length()>0) cast.append(", ");
+							cast.append(actor.getName());
+							if (!StringUtils.isEmpty(castMember.getCharacterName()))
+							{
+								cast.append(" (");
+								cast.append(castMember.getCharacterName());
+								cast.append(")");
+							}
+						}
+					}
+					template.setVariable("cast", cast);
+					File file=File.createTempFile("cover", ".odt");
+					file.deleteOnExit();
+					template.createDocument(file);
+					Utils.run("cmd /c start \"Cover\" \""+file.getAbsolutePath()+"\"", null, null);
+				}
+			}
+		}
+		catch (Exception e1)
+		{
+			GuiUtils.handleThrowable(frame, e1);
+		}
+	}
+}
